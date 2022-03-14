015ab5d9498ff9ad3ad2c285a85c76a11c5fb025
HsuanHsuan <100360425+a1093306@users.noreply.github.com>
2022-03-14 23:57:43 +0800

    homework1

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+</head>
+<body>
+    <?php
+    $i=10;
+    $j=20;
+    echo"<b>Hello</b> <h1>PHP</h1>";
+    echo "<br/>";
+    echo$i+$j;
+    echo"<br/>";
+    echo date("Y,m,d");
+    #echo date("今天是西元Y年m月d日");   #大小寫有差
+    ?>
+</body>
+</html>
